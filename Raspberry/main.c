e5e7e81547e9e6dea879a3dc957338de9952c5eb
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-11-24 09:14:15 -0300

    🏗️ Separate code in smaller parts

--- Raspberry/main.c
+++ Raspberry/main.c
@@ -8,63 +8,11 @@
 #include "utils.h"            // await
 
 
-/**
- * @brief Get the Substring object
- *
- * @param dst Destination variable
- * @param src String to get a substring
- * @param start Initial position of the substring
- * @param end Ending position of the substring
- * @return char* Substring
- */
-char *getSubstring(char *dst, const char *src, size_t start, size_t end) {
-    return strncpy(dst, src + start, end);
-}
-
-/**
- * @brief Separa um argumento lido em 3 substrings
- *
- * @param str string lida
- * @param a priemira substring de tamanho 10
- * @param b segunda substring de tamanho 10
- * @param c terceira substring de tamanho 10
- */
-void separate_string_in_3(char *str, char a[10], char b[10], char c[10]) {
-    char *ptr = strtok(str, ".");
-    int index = 0;
-
-        while(ptr != NULL && index < 3)
-        {
-                //printf("%s\n", ptr);
-                //res[i] = ptr;
-                //strcpy(res[index], ptr);
-                if (index == 0) {
-                    for (int i = 0; i < strlen(ptr) && i < 10; i++) {
-                    //printf("%c", ptr[i]);
-                    a[i] = ptr[i];
-                }
-                a[10] = '\0';
-                }
-                if (index == 1) {
-                    for (int i = 0; i < strlen(ptr) && i < 10; i++) {
-                    //printf("%c", ptr[i]);
-                    b[i] = ptr[i];
-                }
-                b[10] = '\0';
-                }
-                if (index == 2) {
-                    for (int i = 0; i < strlen(ptr) && i < 10; i++) {
-                    //printf("%c", ptr[i]);
-                    c[i] = ptr[i];
-                }
-                c[10] = '\0';
-
-                }
-                //printf("\n");
-                ptr = strtok(NULL, ".");
-                index++;
-        }
-}
+int get_number_of_digital_ios();
+char *getSubstring(char *dst, const char *src, size_t start, size_t end);
+void separate_string_in_3(char *str, char a[10], char b[10], char c[10]);
+void print_io_name_and_id(int max_digital);
+int add_digital_sensor(char *sensor_info, Sensor *digital);
 
 /**
  * @brief Realiza a leitura de sensores de forma automatizada atraves de comunicação serial
@@ -81,6 +29,8 @@ int main(int argc, char *argv[]) {
     init_display();
     clear_display(); // Limpa o conteudo do diaplay
     write_string("Iniciando");
+    uart_configure(); // configura a uart
+    unsigned char ler[100]; // Leitura de respostas
 
     Sensor analogico;
     Sensor digital[31];
@@ -112,133 +62,40 @@ int main(int argc, char *argv[]) {
             analogico.type = Analogic;
         }
         char str[2];
-        int isDigital = strcmp(getSubstring(str, argv[index], 0, 2), "-d");
+        int isDigital = strcmp(getSubstring(str, argv[index], 0, 2), "-d") == 0;
         /**
          * Se inserido uma porta digital
          */
-        if (isDigital == 0) {
+        if (isDigital) {
             //char name[10];
             if (digitalQtd >= 31) {
                 printf("Atingido maximo de sensores digitais!\n");
                 continue;
             }
-            printf("Há um sensor na porta digital\n");
-            char arr[3][11] = {"", "", ""};
-            separate_string_in_3(argv[index], arr[0], arr[1], arr[2]);
-
-            /**
-             * @brief Os parametros para adiconar um sensor digital não
-             * podem ser vazios. O nome e o endereço são obrigatórios
-             */
-            if (strlen(arr[0]) == 0 || strlen(arr[1]) == 0 || strlen(arr[2]) == 0) {
-                printf("\nComando ilegal!!!\n\n");
-                return 0;
-            }
-
-            /**
-             * O endereço do sensor nunca é zero
-             */
-            int address = atoi(arr[2]);
-            if (address == 0) {
-                printf("Endereço invalido para o sensor!!!\n");
-                return 0;
-            }
-
-            /**
-             * @brief Salvando o nome do sensor
-             */
-            for (int i = 0; i < strlen(arr[1]) && i < 10; i++) {
-              //printf("%c", ptr[i]);
-              digital[digitalQtd].name[i] = arr[1][i];
-            }
-            int end = 10;
-            if (strlen(arr[1]) < 10)
-              end = strlen(arr[1]);
-                    digital[digitalQtd].name[end] = '\0';
-            //digital[0].name = arr[1];
-            /**
-             * @brief Definido as outras propriedades do sensor
-             * 
-             */
-            digital[digitalQtd].value = 0;
-            digital[digitalQtd].id = atoi(arr[2]);
-            digital[digitalQtd].type = Digital;
-
-
-            //printf("cmd: %s\n", digital[0].name);
-            printf("Nome: %s\n", digital[digitalQtd].name);
-            printf("Endereço: %i\n", digital[digitalQtd].id);
-
-            /**
-             * Limpando substrings
-             */
-            for (int i = 0; i < 11; i++) {
-                    arr[0][i] = '\0';
-                    arr[1][i] = '\0';
-                    arr[2][i] = '\0';
-                }
-
-                //printf("nome %s", arr[1]);
-            digitalQtd++;
+            int res = add_digital_sensor(argv[index], &digital[digitalQtd]);
+            if (res == -1) { // se hoube erro
+              exit(0);        // termina o programa
+            }                 // do contrario      
+            digitalQtd++;     // contabilize o sensor adicionado
+            
         }
 
         /**
          * @brief Se listando os sensores disponíveis
          */
-        /*if (strcmp(argv[index], "-l") == 0) {
-          char command[2] = "OO";  // Envio de comandos
+        if (strcmp(argv[index], "-l") == 0) {
           unsigned char read[100]; // Leitura de respostas
-          printf("\nPegando o numero de portas digitais disponíveis: \n");
-          uart_send_string("!!");
-          sleep(3);
-          serialReadBytes(read);
-          if (read[0] != '!' || strlen(read) < 2) {
+          //printf("\nPegando o numero de portas digitais disponíveis: \n");
+          int max_digital = get_number_of_digital_ios();
+          if (max_digital < 0) {
             printf("Erro na leitura do numero de portas disponíveis\n");
-            return 0;
+            return 0; // encerra programa
           }
           /** Numero de portas digitais disponíveis */
-          /*int max_digital = read[1] - '0';
-          printf("Portas digitais disponíveis: %i\n\n", max_digital);
-          printf("%10s: %8s\n", "Nome", "Endereço\n");
-          for (int p = 1; p <= max_digital; p++) {
-            command[0] = 'S';           // Obetem o endereço do sensor
-            command[1] = (char) p;  // na posição n°
-            uart_send_string( (char*) command);
-            printf("Obter endereço comando: %s\n", (char*) command);
-            sleep(3);
-            serialReadBytes(read);
-
-            /** Se problema com a node, encerra a leitura*/
-            /*if (read[0] == 31) {
-              printf("NodeMCU com problema!!!\n\n");
-              clear_display();
-              write_string("NodeMCU com erro");
-              return 0;
-            }
-
-            unsigned char sensor_address = read[1]; //    endereço do sensor
-
-            /** Obtendo o nome */
-            /*command[0] = 'N';
-            command[1] = (char) sensor_address;
-            uart_send_string((char*) command);
-            sleep(3);
-            serialReadBytes(read);
-
-            command[0] = read[0];
-            command[1] = read[1];
-
-            // exibe a informação
-            printf("%10s: %2i\n", command, sensor_address);
-          }
-        }*/
+          print_io_name_and_id(max_digital);
+        }
 
     }
-
-    uart_configure(); // configura a uart
-    int input = 0;
-    char cmd[2] = "OO";  // Envio de comandos
-    unsigned char ler[100]; // Leitura de respostas
 
     printf("Iniciando leitura\n\n");
 
@@ -277,4 +134,171 @@ int main(int argc, char *argv[]) {
         return 0;
       }
     }
+}
+
+
+int get_number_of_digital_ios() {
+  unsigned char read[100];
+  send_command('!', '!');
+  await(3000);
+  serialReadBytes(read);
+  if (read[0] != '!' || strlen(read) < 2) {
+    return -1;
+  }
+  /** Numero de portas digitais disponíveis */
+  return read[1] - '0';
+}
+
+void print_io_name_and_id(int max_digital) {
+  unsigned char read[100];
+  char command[100];
+  printf("Portas digitais disponíveis: %i\n\n", max_digital);
+  printf("%10s: %8s\n", "Nome", "Endereço\n");
+  for (int p = 1; p <= max_digital; p++) {
+    send_command(GET_SENSOR_ADDRESS, p);
+    await(3000);
+    serialReadBytes(read);
+
+    /** Se problema com a node, encerra a leitura*/
+    if (read[0] == 31) {
+      printf("NodeMCU com problema!!!\n\n");
+      clear_display();
+      write_string("NodeMCU com erro");
+      exit(0); // termina o programa
+    }
+
+    unsigned char sensor_address = read[1]; //    endereço do sensor
+
+    /** Obtendo o nome */
+    send_command(GET_SENSOR_NAME, sensor_address);
+    await(3000);
+    serialReadBytes(read);
+
+    command[0] = read[0];
+    command[1] = read[1];
+
+    // exibe a informação
+    printf("%10s: %2i\n", command, sensor_address);
+  }
+}
+
+
+/**
+ * @brief Get the Substring object
+ *
+ * @param dst Destination variable
+ * @param src String to get a substring
+ * @param start Initial position of the substring
+ * @param end Ending position of the substring
+ * @return char* Substring
+ */
+char *getSubstring(char *dst, const char *src, size_t start, size_t end) {
+    return strncpy(dst, src + start, end);
+}
+
+/**
+ * @brief Separa um argumento lido em 3 substrings
+ *
+ * @param str string lida
+ * @param a priemira substring de tamanho 10
+ * @param b segunda substring de tamanho 10
+ * @param c terceira substring de tamanho 10
+ */
+void separate_string_in_3(char *str, char a[10], char b[10], char c[10]) {
+  char *ptr = strtok(str, ".");
+  int index = 0;
+
+  while(ptr != NULL && index < 3)
+  {
+    //printf("%s\n", ptr);
+    //res[i] = ptr;
+    //strcpy(res[index], ptr);
+    if (index == 0) {
+      for (int i = 0; i < strlen(ptr) && i < 10; i++) {
+        //printf("%c", ptr[i]);
+        a[i] = ptr[i];
+      }
+      a[10] = '\0';
+    }
+    if (index == 1) {
+      for (int i = 0; i < strlen(ptr) && i < 10; i++) {
+        //printf("%c", ptr[i]);
+        b[i] = ptr[i];
+      }
+      b[10] = '\0';
+    }
+    if (index == 2) {
+      for (int i = 0; i < strlen(ptr) && i < 10; i++) {
+        //printf("%c", ptr[i]);
+        c[i] = ptr[i];
+      }
+      c[10] = '\0';
+    }
+    //printf("\n");
+    ptr = strtok(NULL, ".");
+    index++;
+  }
+}
+
+int add_digital_sensor(char *sensor_info, Sensor *digital) {
+  printf("Há um sensor na porta digital\n");
+  char arr[3][11] = {"", "", ""};
+  separate_string_in_3(sensor_info, arr[0], arr[1], arr[2]);
+
+  /**
+   * @brief Os parametros para adiconar um sensor digital não
+   * podem ser vazios. O nome e o endereço são obrigatórios
+   */
+  if (strlen(arr[0]) == 0 || strlen(arr[1]) == 0 || strlen(arr[2]) == 0) {
+      printf("\nComando ilegal!!!\n\n");
+      //exit(0);
+      return -1;
+  }
+
+  /**
+   * O endereço do sensor nunca é zero
+   */
+  int address = atoi(arr[2]);
+  if (address == 0) {
+      printf("Endereço invalido para o sensor!!!\n");
+      //exit(0);
+      return -1;
+  }
+
+  /**
+   * @brief Salvando o nome do sensor
+   */
+  for (int i = 0; i < strlen(arr[1]) && i < 10; i++) {
+    //printf("%c", ptr[i]);
+    digital->name[i] = arr[1][i];
+  }
+  int end = 10;
+  if (strlen(arr[1]) < 10)
+    end = strlen(arr[1]);
+          digital->name[end] = '\0';
+  //digital[0].name = arr[1];
+  /**
+   * @brief Definido as outras propriedades do sensor
+   * 
+   */
+  digital->value = 0;
+  digital->id = atoi(arr[2]);
+  digital->type = Digital;
+
+
+  //printf("cmd: %s\n", digital[0].name);
+  printf("Nome: %s\n", digital->name);
+  printf("Endereço: %i\n", digital->id);
+
+  /**
+   * Limpando substrings
+   */
+  for (int i = 0; i < 11; i++) {
+          arr[0][i] = '\0';
+          arr[1][i] = '\0';
+          arr[2][i] = '\0';
+      }
+
+      //printf("nome %s", arr[1]);
+  return 1;
 }
